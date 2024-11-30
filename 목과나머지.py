3d2414f4fdc90a125166b2ba4cbe6dde68fd8c1a
BunnyByee <kimhime714@naver.com>
2024-11-30 09:01:41 +0900

    첫번째 업로드

---
+++ 목과나머지.py
@@ -0,0 +1,11 @@
+# print("hello world !") ctrl + / >> 자동 한줄 주석처리
+print("hello","Python",1 ,2, "asdrf", sep=" a ") # sep=""
+print()
+print("1")
+print("안녕하세요 ", end="")
+print("코딩온입니다.")
+
+ive = "I AM"
+print(ive)
+ive ="장원영"
+print(ive)
