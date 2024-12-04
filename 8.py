b437f90af02f6406b4b591cc9155b61d61aacdea
BunnyByee <kimhime714@naver.com>
2024-12-04 13:53:05 +0900

    6th

--- 8.py
+++ 8.py
@@ -1,90 +1,90 @@
-# i = 0
-# while i<5 :
-#     i+=1
-#     print(i)
-# print("끝")
+i = 0
+while i<5 :
+    i+=1
+    print(i)
+print("끝")
 
-# # 무한루프 실행 시 종료하는 법은 ctrl + c
+# 무한루프 실행 시 종료하는 법은 ctrl + c
 
-# # 1-10까지 홀수만 출력
-# n = 0
-# while n < 10 :
-#     n += 1
-#     if n % 2 != 0 :
-#         print(n)
+# 1-10까지 홀수만 출력
+n = 0
+while n < 10 :
+    n += 1
+    if n % 2 != 0 :
+        print(n)
 
-# i = 1
-# while i <10 :
-#     print(i)
-#     i+=2
-# # 이 코드가 더 효율적임!
+i = 1
+while i <10 :
+    print(i)
+    i+=2
+# 이 코드가 더 효율적임!
 
 # 실습 1
-# sum = 0
-# sum_odd = 0
+sum = 0
+sum_odd = 0
 
-# n = int(input("어디까지 계산할까요? "))
-# for i in range (1,n+1) :
-#     sum += i
-#     if i % 2 != 0 :
-#         sum_odd += i
-# print(sum)
-# print(sum_odd)
+n = int(input("어디까지 계산할까요? "))
+for i in range (1,n+1) :
+    sum += i
+    if i % 2 != 0 :
+        sum_odd += i
+print(sum)
+print(sum_odd)
 
-# # 실습 2
-# n = int(input("몇 초?: "))
-# for i in range (n,0,-1) :
-#     print(i, end=" ")
-# print("발사!!")
+# 실습 2
+n = int(input("몇 초?: "))
+for i in range (n,0,-1) :
+    print(i, end=" ")
+print("발사!!")
 
-# # while의 관점에서도 한 번 생각해보기
+# while의 관점에서도 한 번 생각해보기
 
-# # 실습 3
-# dan = int(input("몇 단을 출력할까요? "))
+# 실습 3
+dan = int(input("몇 단을 출력할까요? "))
 
-# for i in range(1,10) :
-#     print(f"{dan} * {i} = {dan*i}")
+for i in range(1,10) :
+    print(f"{dan} * {i} = {dan*i}")
 
-# a = [1,2,3,4,5]
-# a2 = []
-# a3 = []
-# a4 = []
+a = [1,2,3,4,5]
+a2 = []
+a3 = []
+a4 = []
 
-# a3 = [i * 3 for i in a]
-# print(a3)
+a3 = [i * 3 for i in a]
+print(a3)
 
-# a4 = [i*3 for i in a if i%2 == 0]
-# print(a4)
+a4 = [i*3 for i in a if i%2 == 0]
+print(a4)
 
-# for y in range(3) :
-#     for x in range(2) :
-#         print (f"y: {y}, x: {x}")
-#     print()
+for y in range(3) :
+    for x in range(2) :
+        print (f"y: {y}, x: {x}")
+    print()
 
-# # 구구단 전체
-# for i in range(2,10) :
-#     print(f'{i} 단')
-#     for j in range(1, 10):
-#         print(f'{i} x {j} = {i*j}')
-#     print()
+# 구구단 전체
+for i in range(2,10) :
+    print(f'{i} 단')
+    for j in range(1, 10):
+        print(f'{i} x {j} = {i*j}')
+    print()
 
-# # 좌석 배치도 (행)
-# print('*** 자리 배치도 ***')
-# customer = int(input('고객 수 입력: '))
-# row = int(input('좌석행 수 입력: '))
+# 좌석 배치도 (행)
+print('*** 자리 배치도 ***')
+customer = int(input('고객 수 입력: '))
+row = int(input('좌석행 수 입력: '))
 
-# if customer % row == 0 :# 나머지 X 면
-#     column = customer // row  # 몫으로 열을 나누면 됨 ex) 
-# else :
-#     column = (customer // row) + 1 # 아니면 몫+1 더 추가
+if customer % row == 0 :# 나머지 X 면
+    column = customer // row  # 몫으로 열을 나누면 됨 ex) 
+else :
+    column = (customer // row) + 1 # 아니면 몫+1 더 추가
 
-# for i in range(0, row) : # 행 먼저
-#     for j in range(1, column +1) : # 행에 맞춰 열에 사람 세우기
-#         seat = i * column + j
-#         if seat > customer :
-#             break
-#         print(seat, end= ' ')
-#     print()
+for i in range(0, row) : # 행 먼저
+    for j in range(1, column +1) : # 행에 맞춰 열에 사람 세우기
+        seat = i * column + j
+        if seat > customer :
+            break
+        print(seat, end= ' ')
+    print()
 
 # 실습 4-1
 n = int(input('몇 줄? > '))
--- 9.py
+++ 9.py
@@ -17,6 +17,59 @@ count = len(numbers) - 2
 
 print(f"나머지 값의 평균 = {hap/count}")
 
+t1 = (10,20,30)
+print(type(t1))
+print(t1)
+print(t1[0])
+# del t1[0] 튜플 요소 삭제X.
+# t1[0] = 3 튜플 요소 변경X.
+del t1
+
+t2 = (10)
+t3 = (10,)
+t4 = 10, 20
+print(type(t4))
+
+# set 중복된 문자는 한번만 저장 => 중복X 라고 하면 set 사용하면 됨.
+set1 = {1,2,3,3}
+print(set1)
+set2 = set([1,2,3,3,4])
+print(set2)
+set2.add(5)
+print(set2)
+
+while len(set2) > 0 :
+    a = set2.pop() # 랜덤
+    print(a)
+
+l1 = [1,2,3,4]
+while len(l1) > 0 :
+    a = l1.pop() # 마지막 것이 나옴.
+    print(a)
+
+set3 = set('apple')
+print(set3)
+while len(set3) > 0 :
+    a = set3.pop()
+    print(a)
+
+name = ['1','2','3','2']
+a = []
+
+for i in range(0, len(name)):
+    if a.count(name[i]) < 1 :
+        a.append(name[i])
+print(a)
+
+# 중복값 구하기
+for i in range(len(name)):
+    if name.count(name[i]) > 1 :
+        print(name[i])
 
 
 
+name_set = set(name)
+print(name_set)
+name_list = list(name_set)
+name_list.sort()
+print(name_list)
